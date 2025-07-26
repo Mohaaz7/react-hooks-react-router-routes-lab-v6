1e658ab86ba87617f9b2d447da112fe79649b083
mohaaz12 <mohaaz3004@gmail.com>
2025-07-26 13:02:30 +0300

    route-lab

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,24 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Directors from "./pages/Directors";
+import Actors from "./pages/Actors";
+import ErrorPage from "./pages/ErrorPage";
 
-
-const routes = [
-   
-  ];
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/directors", element: <Directors /> },
+      { path: "/actors", element: <Actors /> },
+      { path: "/movie/:id", element: <Movie /> },
+    ],
+  },
+]);
 
 export default routes;
